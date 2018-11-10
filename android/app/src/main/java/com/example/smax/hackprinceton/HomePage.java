ce8b818113faadf1bec1c4cd86b6688f288a1681
smax253 <smax253@gmail.com>
2018-11-09 22:57:27 -0500

    styling the home page

--- android/app/src/main/java/com/example/smax/hackprinceton/HomePage.java
+++ android/app/src/main/java/com/example/smax/hackprinceton/HomePage.java
@@ -2,6 +2,7 @@ package com.example.smax.hackprinceton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        TextView welcomeBanner = (TextView) findViewById(R.id.welcomeBanner);
+        welcomeBanner.setText("Welcome to Princeton");
     }
 }
